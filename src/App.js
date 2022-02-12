ad1c0ff9b2eaa9764bae2ddcd55e3237de57c57e
AdrianKauling <adriankauling2@gmail.com>
2022-02-11 22:22:16 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+import React,{ useState } from 'react'
+
+export default function App() {
+
+  const [ peso, setPeso ] = useState('')
+  const [ altura, setAltura ] = useState('')
+  const [ mensagem, setMensagem ] = useState('')
+
+  function calcularImc() {
+    const altM2 = altura/100
+    const imc = peso / (altM2 * altM2)
+
+    if( imc < 18.5 ){
+
+      setMensagem('Alerta de magreza - imc: ' + imc.toFixed(2))
+
+    }else if( imc >= 18.5 && imc <= 24.9 ){
+
+      setMensagem('Normal - imc: ' + imc.toFixed(2))
+
+    }else if( imc >= 24.9 &&  imc <= 30.0 ){
+
+      setMensagem('Sobrepeso - imc: ' + imc.toFixed(2))
+
+    }else if( imc > 30.0){
+
+      setMensagem('Alerta de obesidade - imc: ' + imc.toFixed(2))
+
+    }
+
+
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="app">
+      <h1>Calculadora imc</h1>
+      <span>Vamos calcular seu imc</span>
+      <input 
+        type="text"
+        placeholder="Seu peso (kg) Ex: 74"
+        value={peso}
+        onChange = { event => setPeso(event.target.value)}
+      ></input>
+      <input 
+        type="text"
+        placeholder="Sua altura (cm) Ex: 174"
+        value={altura}
+        onChange = { event => setAltura(event.target.value)}
+      ></input>
 
-export default App;
+      <button
+        onClick = {calcularImc}
+      >
+      Calcular
+      </button>
+
+      <h2
+      >{mensagem}</h2>
+    </div>
+
+    
+  )
+}
